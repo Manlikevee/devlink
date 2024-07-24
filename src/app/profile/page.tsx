1220afdf8a65c1cbbb259ca334af7557f8491a49
manlikevee <odahviktor@gmail.com>
2024-07-24 16:15:09 +0100

    SACSA

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,206 @@
+'use client'
+import Layout from '@/components/Layout'
+import Topnav from '@/components/Topnav'
+import Emptystatesocials from '@/components/utility/Emptystatesocials'
+import Pageheading from '@/components/utility/Pageheading'
+import Socialinputbox from '@/components/utility/Socialinputbox'
+import React, { useState } from 'react';
+import { Github, Youtube, Linkedin, Facebook, Codepen, ChevronRight, ImageUp } from 'lucide-react';
+import Inputbox from '@/components/utility/Inputbox'
+
+
+const options = [
+  { value: 'github', label: 'GitHub', icon: <Github size={16} /> },
+  { value: 'youtube', label: 'YouTube', icon: <Youtube size={16} /> },
+  { value: 'linkedin', label: 'LinkedIn', icon: <Linkedin size={16} /> },
+  { value: 'facebook', label: 'Facebook', icon: <Facebook size={16} /> },
+  { value: 'frontendmentor', label: 'Frontend Mentor', icon: <Codepen size={16} /> },
+];
+
+
+const page = () => {
+    const [email, setEmail] = useState('');
+  const [socialInputs, setSocialInputs] = useState<{ platform: string; link: string }[]>([]);
+
+  const [image, setImage] = useState<string | null>(null);
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleClick = () => {
+    const fileInput = document.getElementById('fileInput');
+    if (fileInput) {
+      fileInput.click();
+    }
+  };
+
+  return (
+    <Layout>
+<div className="container">
+
+
+<Topnav/>
+
+<div className="heroflex">
+<div className="heroflexone">
+
+<div className="preview">
+  <div className="previewblock">
+    <div className="avatar"></div>
+   <div className="uname"></div>
+   <div className="uemail"></div>
+   {socialInputs.map((input, index) => (
+  (input.platform && input.link.trim() ) ? (
+    <div className={`socialbox ${input.platform}`} key={index}>
+   <span className='aic'>     
+        {options.find(option => option.value == input.platform)?.icon}
+            <span style={{ marginLeft: 3 }}>
+               {input.platform} 
+               </span>    
+               </span>
+
+               <span style={{ marginTop: 3 }}><ChevronRight size={'14'} /></span>
+    </div>
+  ) : null
+))}
+   {/* <div className="socialbox"></div>
+   <div className="socialbox"></div>
+   <div className="socialbox"></div>
+   <div className="socialbox"></div>
+   <div className="socialbox"></div> */}
+  </div>
+</div>
+
+</div>
+
+<div className="heroflextwo">
+
+<Pageheading title='Profile Details' subtitle='Add your details to create a personal touch to your profile.'/>
+
+
+<div className="socialblock">
+
+
+<div className="socialblockbox">
+    <div className="profilephotoflex aic">
+<div className="ppbox">Profile picture</div>
+<div className="ppbox centered">
+    <div>
+        
+    </div>
+
+    <div className="fileupload centered" onClick={handleClick} style={{ position: 'relative', cursor: 'pointer' }}>
+      <input
+        type="file"
+        id="fileInput"
+        accept="image/*"
+        style={{ display: 'none' }}
+        onChange={handleImageUpload}
+      />
+      {image ? (
+        <>
+          <img src={image} alt="Uploaded" style={{ width: '100%', height: '100%' }} />
+          <div className="overlay" style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            color: 'white',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            opacity: 1,
+            transition: 'opacity 0.3s'
+          }}>
+               <ImageUp />
+          + Change Image
+          </div>
+        </>
+      ) : (
+        <div className="placeholder">
+          <ImageUp />
+          + Upload Image
+        </div>
+      )}
+    </div>
+
+</div>
+<div className="ppbox"> <small>Image must be below 1024x1024px. Use PNG or JPG format.</small> </div>
+    </div>
+</div>
+
+<div className="socialblockbox">
+<div className="inputflex">
+    <div className="inputlabel">First name*</div>
+    <Inputbox
+          inputState={email} 
+          setInputState={setEmail} 
+          label="" 
+          inputType="email" 
+          name="email" 
+          id="email" 
+        //   icon = {Mail}
+          placeholder='e.g. alex@email.com'
+          />
+
+</div>
+<div className="inputflex">
+    <div className="inputlabel">Last name*</div>
+    <Inputbox
+          inputState={email} 
+          setInputState={setEmail} 
+          label="" 
+          inputType="email" 
+          name="email" 
+          id="email" 
+        //   icon = {Mail}
+          placeholder='e.g. alex@email.com'
+          />
+
+</div>
+<div className="inputflex">
+    <div className="inputlabel">Email*</div>
+    <Inputbox
+          inputState={email} 
+          setInputState={setEmail} 
+          label="" 
+          inputType="email" 
+          name="email" 
+          id="email" 
+        //   icon = {Mail}
+          placeholder='e.g. alex@email.com'
+          />
+
+</div>
+</div>
+
+
+</div>
+
+
+
+<div className="pagefooter">
+ <button className='mybtn'>Save</button>
+</div>
+</div>
+</div>
+
+</div>
+
+
+    </Layout>
+
+  )
+}
+
+export default page
--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
-
+import { Link , CircleUser } from 'lucide-react';
+import { usePathname,  } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 const Topnav = () => {
+  const router = useRouter()
+  const pathname = usePathname();
+
+  
+  const handlecorp = () => {
+    router.push('/profile');
+  };
+
+  const handlehome = () => {
+    router.push('/');
+  };
   return (
 
     <header>
     <div className='topnav'>
-        <img src="/Group251.png" alt="" />
+        <img src="/Group251.png" alt=""  className='desktop'/>
+        <img src="/Group2511.png" alt="" className='mobile'/>
+        <div className="navlinks">
+        <div className={`navlink aic  ${pathname === '/' ? 'navlinkactive' : ''}`}   onClick={handlehome}>
+          <Link size={'18'}/> <span>Links</span> 
+          </div>
+          <div className={`navlink aic  ${pathname === '/profile' ? 'navlinkactive' : ''}`} onClick={handlecorp}>
+          <CircleUser  size={'18'}/> <span>Profile Details</span> 
+          </div>
+        </div>
 
-        <div className="navlinks"></div>
-
-        <div className="navbtn"></div>
+        <div className="navlink navbtn">
+          Preview
+        </div>
     </div>
     </header>
 
